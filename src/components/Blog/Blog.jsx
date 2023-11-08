b9020a0c42833b345ab55d70620ef64b07faf0dc
shawon13 <rakibarg1519@gmail.com>
2023-11-08 23:06:36 +0600

    some blog update

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
+import './Blog.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 const Blog = ({ blog }) => {
-    console.log(blog)
     return (
-        <div className="card">
-            <img src={blog.img} className="card-img-top" alt="..." />
-            <div className="card-body">
-                <div className="">
-                    <div className="">
-                        <img src={blog.author_img} alt="" />
-                    </div>
+        <div className="blog">
+            <img src={blog.img} className="blog-img" alt="..." />
+            <div className="blog-body">
+                <div className="author-details">
                     <div>
-                        <h3 className="">{blog.author}</h3>
-                        <p>{blog.publish_date}</p>
+                        <img className="author-img" src={blog.author_img} alt="" />
+                    </div>
+                    <div style={{ marginLeft: '15px' }}>
+                        <h6 className="author-name">{blog.author}</h6>
+                        <p className='publish-date'>{blog.publish_date}</p>
                     </div>
                 </div>
-                <div className="">
-                    <p>{blog.read_time} min read <FontAwesomeIcon icon={faBookmark} /></p>
+                <div>
+                    <p className='read-time'>{blog.read_time} min read <FontAwesomeIcon icon={faBookmark} /></p>
                 </div>
             </div>
-            <div className="card-body">
-                <h2>{blog.name}</h2>
-                <a href="#" className="card-link">Mark as read</a>
+            <div className="blog-footer">
+                <h2 className='blog-name'>{blog.name}</h2>
+                <a href="#" style={{ textDecoration: 'underline' }}>Mark as read</a>
             </div>
         </div>
     );
